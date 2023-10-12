4757b0efd726fab0eef84f0cbed2965255e0b0a8
home-picks <homepicks2023@gmail.com>
2023-10-12 22:13:35 +0900

    モバイルUI変更

--- pages/components/NavMobile.jsx
+++ pages/components/NavMobile.jsx
@@ -18,45 +18,78 @@ export default function Nav({ setNavMobile }) {
         {path === "/" ?
           (<>
             <Scroll to="about" smooth={true} offset={-50}>
-              <li className='text-white hover:text-green-300 duration-300 cursor-pointer'>長町研磨について</li>
+              <li className='text-white cursor-pointer'>長町研磨について</li>
             </Scroll>
             <Scroll to="work" smooth={true} offset={-50}>
-              <li className='text-white hover:text-green-300 duration-300 cursor-pointer'>事業内容</li>
+              <li className='text-white cursor-pointer'>事業内容</li>
             </Scroll>
             <Scroll to="company" smooth={true} offset={-50}>
-              <li className='text-white hover:text-green-300 duration-300 cursor-pointer'>会社概要</li>
+              <li className='text-white cursor-pointer'>会社概要</li>
             </Scroll>
             <Scroll to="contact" smooth={true} offset={-50}>
-              <li className='text-white hover:text-green-300 duration-300 cursor-pointer'>お問い合わせ</li>
+              <li className='text-white cursor-pointer'>お問い合わせ</li>
             </Scroll>
             <Link href='/online-kenma'
-              className='text-white hover:text-green-300 duration-300 rounded border-2 px-2 hover:border-green-500 border-white'>
-              価格表
+              className='text-white rounded border-2 px-2 hover:border-green-500 border-white'>
+              オンライン研磨「価格表」
             </Link>
-            <Link href='/kenma' className='text-white hover:text-green-300 duration-300 rounded border-2 px-2 hover:border-green-500 border-white'>研磨一覧</Link>
+            <Link href='/kenma' className='text-white rounded border-2 px-2 hover:border-green-500 border-white'>
+              研磨一覧
+            </Link>
           </>
-          ) : (
-            <>
-              <Scroll to="slitter" smooth={true} offset={-50}>
-                <li className='text-white hover:text-green-300 duration-300 cursor-pointer'>スリッター刃</li>
-              </Scroll>
-              <Scroll to="funsai" smooth={true} offset={-50}>
-                <li className='text-white hover:text-green-300 duration-300 cursor-pointer'>粉砕/製袋刃</li>
-              </Scroll>
-              <Scroll to="dansai" smooth={true} offset={-50}>
-                <li className='text-white hover:text-green-300 duration-300 cursor-pointer'>断裁刃</li>
-              </Scroll>
-              <Scroll to="other" smooth={true} offset={-50}>
-                <li className='text-white hover:text-green-300 duration-300 cursor-pointer'>その他刃物</li>
-              </Scroll>
-              <Link href='/online-kenma'
-                className='text-white hover:text-green-300 duration-300 rounded border-2 px-2 hover:border-green-500 border-white'>
-                価格表
-              </Link>
-              <Link href='/' className='text-white hover:text-green-300 duration-300 rounded border-2 px-2 hover:border-green-500 border-white'>長町研磨について</Link>
-            </>)
-        }
+          ) : (<></>)}
+
+        {path === "/kenma" ?
+          (<>
+            <Scroll to="slitter" smooth={true} offset={-50}>
+              <li className='text-white cursor-pointer'>スリッター刃</li>
+            </Scroll>
+            <Scroll to="funsai" smooth={true} offset={-50}>
+              <li className='text-white cursor-pointer'>粉砕/製袋刃</li>
+            </Scroll>
+            <Scroll to="dansai" smooth={true} offset={-50}>
+              <li className='text-white cursor-pointer'>断裁刃</li>
+            </Scroll>
+            <Scroll to="other" smooth={true} offset={-50}>
+              <li className='text-white cursor-pointer'>その他刃物</li>
+            </Scroll>
+            <Link href='/online-kenma'
+              className='text-white rounded border-2 px-2 hover:border-green-500 border-white'>
+              オンライン研磨「価格表」
+            </Link>
+            <Link href='/' className='text-white rounded border-2 px-2 hover:border-green-500 border-white'>長町研磨について</Link>
+          </>
+          ) : (<></>)}
+
+        {path === "/online-kenma" ?
+          (<>
+            <Scroll to="online" smooth={true} offset={-82}>
+              <li className='text-white cursor-pointer'>
+                オンライン研磨の流れ
+              </li>
+            </Scroll>
+            <Scroll to="slitter" smooth={true} offset={-82}>
+              <li className='text-white cursor-pointer'>
+                スリッター刃
+              </li>
+            </Scroll>
+            <Scroll to="funsai" smooth={true} offset={-82}>
+              <li className='text-white cursor-pointer'>
+                粉砕/チッパー/製袋刃
+              </li>
+            </Scroll>
+            <Scroll to="contact" smooth={true} offset={-50}>
+              <li className='text-white cursor-pointer'>
+                お問い合わせ
+              </li>
+            </Scroll>
+            <Link href='/kenma'
+              className='text-white rounded border-2 px-2 hover:border-green-500 border-white'>
+              研磨一覧
+            </Link>
+          </>
+          ) : (<></>)}
       </ul>
-    </nav>
+    </nav >
   )
 }
